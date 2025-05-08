d5e42a488d005f5d6a16434cae1f469f79a6621d
aset-mo <imercadoortega@byupathway.edu>
2025-05-08 09:11:57 -0500

    Guess number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int magicNumber = RandomNumberGenerator.GetInt32(1, 100); 
+
+
+
+        int guess = -1; 
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+
+            if (!int.TryParse(userGuess, out guess))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                continue;
+            }
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
